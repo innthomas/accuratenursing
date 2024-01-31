dd1c14f50f9c4b6b620db1f7848af21043296f43
innthomas <innthomas00158@gmail.com>
2024-01-31 05:47:25 +0100

    edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 
 //const inter = Inter({ subsets: ["latin"] });
-export const fontSans = FontSans({
+export const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
